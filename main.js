7acce4398e67d4ebedb805bcac54ea9aae2cdcc1
OxanaVoskresenskaya <oavoskresenskaya@mail.ru>
2022-04-22 16:09:18 +0300

    object traning

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-let a=prompt('Первое число?', 1);
-let b=prompt('Второе число?', 2);
-alert(+a+ +b);
+const person={
+    name:"John",
+    lastname:"Doe",
+    work:{
+        workName:"programmer",
+        level:"juniour"
+    },
+    info:"bad worker", 
+}
+function modifyObj(person){
+    delete person.info
+    person.age = 25;
+    person.work.level = 'middle';
+    return person;
+   }
+   let per=modifyObj(person)
+   console.log(per);
