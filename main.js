1c2daba2b74eed00fcd167bf6fa0ba9bcd89b2f3
OxanaVoskresenskaya <oavoskresenskaya@mail.ru>
2022-04-22 17:24:55 +0300

    using conditions

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-const person={
-    name:"John",
-    lastname:"Doe",
-    work:{
-        workName:"programmer",
-        level:"juniour"
-    },
-    info:"bad worker", 
-}
-function modifyObj(person){
-    delete person.info
-    person.age = 25;
-    person.work.level = 'middle';
-    return person;
-   }
-   let per=modifyObj(person)
-   console.log(per);
+let login;
+let password;
+function singIn(login,password){
+    let result;
+    if (login=='friend' && password=='admin201')
+    {result=false;}
+    if(login=='admin' && password=='admin2019')
+    {result=true;}
+    if(login=='root' && password=='admin2019')
+    {result=false;}
+    if(login=='admin' && password=='admin')
+    {result=false;}
+    return result;}
+let res = singIn('root','admin2019');
+console.log (res);
