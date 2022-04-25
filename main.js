b5263a16e5552feb5592b05cf9b4c16ae60cdb8b
Oxana <oavoskresenskaya@mail.ru>
2022-04-25 22:02:45 +0300

    word comparison without includes

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-
-function compared(Word, nextWord) {
-    Word = prompt ('Ведите слово', '');
-    nextWord = prompt ('Ведите следующее слово','');
-    if (Word.includes(nextWord)) {
-        return true;
-    }else {
-        return false;
+function compared (word, nextWord){
+    word = prompt ('Ведите слово', '');
+    nextWord = prompt ('Введите следующее слово', '');
+        word.split('');
+        nextWord.split('');
+    for (let i = 0; i < nextWord.length; i++) {
+    for (let k = 0; k < word.length; k++) {
+        if (nextWord[i]===word[k]) {
+        return true;} 
+        else {
+            return false;}
+        }
     }
 }
-console.log(compared());
-
+alert(compared());
